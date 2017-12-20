ff1a81da6c2618d1a489f6a75d21d230ff3e0011
Ross Dyson <dyson.ross@gmail.com>
2017-12-20 22:44:42 +0800

    getting and storing all rates 🎒

--- getRates.js
+++ getRates.js
@@ -2,7 +2,7 @@
 const BFX = require('bitfinex-api-node');
 const _ = require('lodash');
 
-const symbols = [
+let symbols = [
   'btcusd',
   'ltcusd',
   'ethusd',
@@ -33,32 +33,47 @@ const symbols = [
 ];
 
 module.exports = (ctx, cb) => {
+  console.log('NEW_GET_RATES_REQUEST');
   const apiKey = ctx.secrets.BITFINEX_API_KEY;
   const apiSecret = ctx.secrets.BITFINEX_API_SECRET;
   
   const bfxRest = new BFX(apiKey, apiSecret, {version: 1}).rest;
   
   const getRate = (symbol) => {
+    console.log(`GETTING_RATE_FOR ${symbol}`);
     bfxRest.ticker(symbol, (err, res) => {
       if (err) cb(null, err);
       const rate = res.last_price;
-      // cb(null, rate);
+      console.log(`GOT_RATE_FOR ${symbol} - ${rate}`);
       storeRates(symbol, rate);
     });
   };
   
   const storeRates = (symbol, rate) => {
+    console.log(`STORING_RATE_FOR ${symbol}`);
     ctx.storage.get(function (err, data) {
       if (err) cb(null, err);
-      console.log(data);
       let store = data || {};
       store[symbol] = rate;
+      console.log(`GOT_STORE`);
+      console.log(store);
       ctx.storage.set(store, function (err) {
         if (err) cb(null, err);
-        cb(null, store);
+        console.log(`UPDATED_STORE_WITH ${symbol}`);
+        getRates(store);
       });
     });
   };
   
-  getRate(symbols[0]);
+  const getRates = (store) => {
+    const rateToGet = symbols.shift();
+    if (rateToGet !== undefined) {
+      getRate(rateToGet);
+    } else {
+      console.log('GOT_ALL_RATES');
+      cb(null, store)
+    }
+  };
+  
+  getRates();
 };
