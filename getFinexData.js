9527fdf353c09d146763e3d6abdcea269f42d871
Ross Dyson <dyson.ross@gmail.com>
2017-12-23 00:15:19 +0800

    updating logs to include taskname

--- getFinexData.js
+++ getFinexData.js
@@ -1,6 +1,8 @@
 'use strict';
 const BFX = require('bitfinex-api-node');
 const _ = require('lodash');
+
+let taskName;
 let symbols = [
   'btcusd',
   'ltcusd',
@@ -32,30 +34,28 @@ let symbols = [
 ];
 
 module.exports = (ctx, cb) => {
-  console.log(`NEW_REQUEST`);
   const apiKey = ctx.secrets.BITFINEX_API_KEY;
   const apiSecret = ctx.secrets.BITFINEX_API_SECRET;
   const bfxRest = new BFX(apiKey, apiSecret, {version: 1}).rest;
   
   const getRate = (symbol) => {
-    console.log(`GETTING_RATE: ${symbol}`);
+    console.log(`${taskName} GETTING_RATE: ${symbol}`);
     bfxRest.ticker(symbol, (err, res) => {
       if (err) {
-        console.log(`GET_RATE_ERROR:`);
+        console.log(`${taskName} GET_RATE_ERROR:`);
         console.log(err);
         cb(null, err);
       }
       const rate = res.last_price;
-      console.log(`GOT_RATE: ${symbol} - ${rate}`);
       storeRates(symbol, rate);
     });
   };
   
   const storeRates = (symbol, rate) => {
-    console.log(`STORING_RATE: ${symbol}`);
+    console.log(`${taskName} STORING_RATE: ${symbol} - $${rate}`);
     ctx.storage.get(function (err, data) {
       if (err) {
-        console.log(`GET_STORE_ERROR:`);
+        console.log(`${taskName} GET_RATE_STORE_ERROR:`);
         console.log(err);
         cb(null, err);
       }
@@ -63,11 +63,10 @@ module.exports = (ctx, cb) => {
       store[symbol] = rate;
       ctx.storage.set(store, function (err) {
         if (err) {
-          console.log(`UPDATE_STORE_ERROR:`);
+          console.log(`${taskName} UPDATE_RATE_STORE_ERROR:`);
           console.log(err);
           cb(null, err);
         }
-        console.log(`UPDATED_STORE: ${symbol}`);
         getRates(store);
       });
     });
@@ -78,13 +77,13 @@ module.exports = (ctx, cb) => {
     if (rateToGet !== undefined) {
       getRate(rateToGet);
     } else {
-      console.log('GOT_ALL_RATES');
+      console.log(`${taskName} GOT_ALL_RATES`);
       cb(null, store)
     }
   };
   
   const buildBalance = (wallet, rates) => {
-    console.log('BUILDING_BALANCE');
+    console.log(`${taskName} BUILDING_BALANCE`);
     let totals = {};
     wallet.forEach((walletObj) => {
       const { currency, amount } = walletObj;
@@ -92,7 +91,7 @@ module.exports = (ctx, cb) => {
       const currencyUsd = `${currency}usd`;
       
       if (totals[currency] === undefined) {
-        console.log(`ADDING_NEW_CURRENCY: ${currency} - ${amountNum}`);
+        console.log(`${taskName} ADDING_NEW_CURRENCY: ${currency} - ${amountNum}`);
         totals[currency] = {};
         totals[currency].holding = amountNum;
         if (currency !== 'usd') {
@@ -103,7 +102,7 @@ module.exports = (ctx, cb) => {
         }
       }
       else {
-        console.log(`ADDING_TO_EXISTING_CURRENCY: ${currency} - ${amountNum}`);
+        console.log(`${taskName} ADDING_TO_EXISTING_CURRENCY: ${currency} - ${amountNum}`);
         const currentTotal = totals[currency].holding;
         const newTotal = currentTotal + amountNum;
         totals[currency].holding = newTotal;
@@ -122,34 +121,34 @@ module.exports = (ctx, cb) => {
     res.totalValue = 0;
     _.each(totals, (currencyObj, key) => {
       if (_.isNumber(currencyObj.value) && !_.isNaN(currencyObj.value)) {
-        console.log(`ADDING_TO_TOTAL: ${key} - $${currencyObj.value}`);
+        console.log(`${taskName} ADDING_TO_TOTAL: ${key} - $${currencyObj.value}`);
         res.totalValue += currencyObj.value;
       }
     });
-    console.log('BALANCE_COMPLETE');
+    console.log(`${taskName} BALANCE_BUILD_COMPLETE`);
     cb(null, res);
   };
   
   const getWallet = () => {
     bfxRest.wallet_balances((err, res) => {
       if (err) {
-        console.log(`GET_WALLET_ERROR:`);
+        console.log(`${taskName} GET_WALLET_ERROR:`);
         console.log(err);
         cb(null, err);
       }
-      console.log(`GOT_WALLET`);
+      console.log(`${taskName} GOT_WALLET`);
       ctx.storage.get(function (err, data) {
         if (err) {
-          console.log(`GET_STORE_ERROR:`);
+          console.log(`${taskName} GET_RATE_STORE_ERROR:`);
           console.log(err);
           cb(null, err);
         }
         if (data === {}) {
-          console.log(`STORE_EMPTY`);
-          cb(null, 'STORE_EMPTY');
+          console.log(`${taskName} RATE_STORE_EMPTY`);
+          cb(null, 'RATE_STORE_EMPTY');
         }
         else {
-          console.log(`GOT_STORE`);
+          console.log(`${taskName} GOT_RATE_STORE`);
           buildBalance(res, data);
         }
       });
@@ -157,10 +156,12 @@ module.exports = (ctx, cb) => {
   };
   
   if (ctx.query.summary === 'true') {
-    console.log(`NEW_GET_WALLET_REQUEST`);
+    console.log(`NEW_GET_BALANCES_REQUEST`);
+    taskName = 'GET_BALANCES';
     getWallet()
   } else {
     console.log(`NEW_GET_RATES_REQUEST`);
+    taskName = 'GET_RATES';
     getRates();
   }
 };
